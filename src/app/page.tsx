bebea41c6bf3cf68e2c104d5c11af8bf06c9718f
João Lucas Lopes Dias <joaolucaslopesd@gmail.com>
2023-10-15 21:50:54 -0300

    Estruturada primeira versão da landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,193 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex flex-col w-screen max-w-full bg-white">
+      <header className="flex w-full h-24 bg-[#FA4A0C]">
+        <div className="w-full flex justify-start items-center ml-20">
+          <img
+            src="/images/logo.png"
+            alt="PartyLink"
+            className="w-[180px] h-[50px]"
+          />
+        </div>
+        <nav className="text-white flex w-full justify-end items-center gap-20 mr-20">
+          <a className="hover:text-black transition" href="#sobre">
+            Sobre
+          </a>
+          <a className="hover:text-black transition" href="#download">
+            Download
+          </a>
+        </nav>
+      </header>
+      <section className="w-full h-[620px]">
+        <div
+          className="h-full bg-cover bg-center"
+          style={{ backgroundImage: 'url("/images/banner.jpg")' }}
+        >
+          <div className="w-full h-full bg-black bg-opacity-50 flex flex-col justify-center items-center">
+            <div className="w-[70%] flex justify-center text-center">
+              <h1 className="text-white text-5xl font-bold">
+                Garanta momentos inesquecíveis com as pessoas que você ama!
+              </h1>
+            </div>
+
+            <p className="pt-8 text-white text-xl">
+              Organize suas festas de forma prática e rápida.
+            </p>
+            <button className="bg-[#FA4A0C] text-white px-10 py-3 rounded-md mt-10 hover:bg-[#cc3600] transition">
+              Baixar
+            </button>
+          </div>
+        </div>
+      </section>
+      <section id="sobre" className="w-full h-[700px]">
+        <div className="flex flex-col justify-center items-center w-full mt-10">
+          <div className="w-full h-full flex justify-center items-center">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              alt="Página de cadastro do aplicativo"
+              src="/images/start-mockup.png"
+              width={250}
+              height={584}
+            />
+            <Image
+              alt="Página inicial do aplicativo"
+              src="/images/home-mockup.png"
+              width={250}
+              height={584}
+            />
+          </div>
+          <div className="flex justify-center h-[3px] w-[60%] bg-[#E4E4E4]"></div>
+          <h2 className="mt-10 text-black text-5xl font-semibold">
+            Como o aplicativo funciona?
+          </h2>
+        </div>
+      </section>
+      <section className="w-full mt-20">
+        <div className="flex justify-center items-center">
+          <div className="w-full flex justify-center">
+            <Image
+              alt="Página de Login"
+              src="/images/login-mockup.png"
+              width={261}
+              height={525}
+            />
+          </div>
+          <div className="w-full">
+            <div className="w-[80%]">
+              <p className="text-[#FA4A0C] font-bold text-2xl">
+                Crie uma conta
+              </p>
+              <p className="text-[#252B42] font-bold text-[40px] mt-3 mb-8">
+                {" "}
+                Crie uma conta ou entre em uma já existente
+              </p>
+              <p className="text-2xl font-medium text-[#737373]">
+                Uma conta será criada com o seu email e a senha escolhida!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center my-40">
+          <div className="flex justify-end w-full">
+            <div className="flex flex-col justify-center w-[80%]">
+              <p className="text-[#FA4A0C] font-bold text-2xl">
+                Explore a variedade
+              </p>
+              <p className="text-[#252B42] font-bold text-[40px] mt-3 mb-8">
+                {" "}
+                Encontre produtos e serviços
+              </p>
+              <p className="text-2xl font-medium text-[#737373]">
+                Você pode buscar por produtos específicos ou por categorias de
+                produtos que deseja incluir em sua festa!
+              </p>
+            </div>
+          </div>
+          <div className="w-full flex justify-center">
+            <Image
+              alt="Página de busca de produtos"
+              src="/images/produtos-mockup.png"
+              width={261}
+              height={525}
+            />
+          </div>
+        </div>
+        <div className="flex justify-center items-center mb-20">
+          <div className="w-full flex justify-center">
+            <Image
+              alt="Página de Revisão de Itens e Pagamento"
+              src="/images/payment-mockup.png"
+              width={261}
+              height={525}
+            />
+          </div>
+          <div className="w-full">
+            <div className="w-[80%]">
+              <p className="text-[#FA4A0C] font-bold text-2xl">Checkout</p>
+              <p className="text-[#252B42] font-bold text-[40px] mt-3 mb-8">
+                {" "}
+                Revise os itens e efetue o pagamento
+              </p>
+              <p className="text-2xl font-medium text-[#737373]">
+                Com o PartyLink, você pode revisar todos os seus pedidos e pagar
+                de forma centralizada!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="download" className="w-full h-[500px]">
+        <div
+          className="w-full h-full bg-cover bg-center overflow-hidden"
+          style={{
+            backgroundImage: 'url("/images/bday.jpg")',
+          }}
+        >
+          <div className="w-full h-full bg-black bg-opacity-50 flex flex-col justify-center items-center">
+            <div className="w-[70%] flex justify-center text-center">
+              <h1 className="text-white text-5xl font-bold">
+                Crie memórias com PartyLink. Baixe agora.
+              </h1>
+            </div>
+
+            <p className="pt-8 text-white text-xl">
+              Disponível para Android e iOS.
+            </p>
+            <button className="bg-[#FA4A0C] text-white px-10 py-3 rounded-md mt-10 hover:bg-[#cc3600] transition">
+              Baixar
+            </button>
+          </div>
+        </div>
+      </section>
+      <footer className="flex justify-evenly items-center w-full h-28 bg-[#FA4A0C]">
+        <img
+          src="/images/logo.png"
+          alt="PartyLink"
+          className="w-[180px] h-[50px]"
+        />
+        <div className="flex gap-10">
+          <a href="">
+            <Image
+              alt="Logo do Facebook"
+              src="/images/facebook.svg"
+              width={40}
+              height={40}
+            />
+          </a>
+          <a href="">
+            <Image
+              alt="Logo do Instagram"
+              src="/images/instagram.svg"
+              width={40}
+              height={40}
             />
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <p className="text-xs text-[#5c5c5c] font-bold">
+          Copyright 2023 PartyLink
+        </p>
+      </footer>
     </main>
-  )
+  );
 }
